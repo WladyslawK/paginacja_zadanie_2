b79cb915a9f0dd095be0bcf43f047115ed2040a4
WladyslawK <vladkurr7890@gmail.com>
2023-02-25 23:20:21 +0100

    feat(usePagination) usePagination refactor

--- src/sample/usePagination.ts
+++ src/sample/usePagination.ts
@@ -1,25 +1,56 @@
-import {useMemo} from "react";
+import {useState} from "react";
+import {postTpe} from "./Pagination";
 
-type usePaginationType = {
-  totalCount: number
-  pageSize: number
-  siblingCount?: number
-  currentPage: number
+type paginationStateType = {
+  actualPageIdx: number
+  lastPageIdx: number
+  entriesOnSelectedPage: number
 }
 
-export const usePagination: (data: usePaginationType) => void = ({
-                                                                   totalCount, pageSize, siblingCount= 1, currentPage
+type paginationActionsType = {
+  goToFirstPage: () => void
+  goToPrevPage: () => void
+  goToPage: (pageIdx: number) => void
+  goToNextPage: () => void
+  goToLastPage: () => void
+  changeEntriesOnPage: (entries: number) => void
+}
 
-                                                                 }) => {
+export const usePagination = (dataEntries: postTpe[], elementsOnPage: number = 10): [paginationState: paginationStateType, paginationActions: paginationActionsType] => {
+
+  const [actualPageIdx, setActualPageIdx] = useState(1)
+  const [entriesOnSelectedPage, setEntriesOnSelectedPage] = useState(10)
+
+  const lastPageIdx = Math.ceil(dataEntries.length / elementsOnPage)
 
 
-  const paginationRange = useMemo(() => {
-  }, [])
+  //go to firstPage callback
+  const goToFirstPage = () => setActualPageIdx(1)
 
-  
+  //go to Page
+  const goToPage = (pageIdx: number) => setActualPageIdx(pageIdx)
+
+  //go to next Page
+  const goToNextPage = () => setActualPageIdx(prevState => prevState + 1)
+
+  //go to previous page
+  const goToPrevPage = () => setActualPageIdx(prevState => prevState - 1)
+
+  // go to last page
+  const goToLastPage = () => setActualPageIdx(lastPageIdx)
+
+  //change Entries on page
+  const changeEntriesOnPage = (entries: number) => setEntriesOnSelectedPage(entries)
+
+  const paginationState: paginationStateType = {
+    actualPageIdx,
+    lastPageIdx,
+    entriesOnSelectedPage
+  }
+  const paginationActions: paginationActionsType = {goToFirstPage, goToPrevPage, goToPage, goToNextPage, goToLastPage, changeEntriesOnPage}
 
 
+  return [paginationState, paginationActions]
 
-
-
+  // return [paginationState, paginationActions]
 }
