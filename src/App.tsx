639e61775e9f66581a85100e68a7176996f7bc4e
WladyslawK <vladkurr7890@gmail.com>
2023-02-25 23:36:22 +0100

    feat(PaginatedData) PaginatedData

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-import {PaginatedTable} from "./components/paginatedTable/PaginatedTable";
-import {Pagination} from "./sample/Pagination";
+import {Pagination} from "./components/pagination/Pagination";
 
 function App() {
   return (
     <div className="App">
-        Paginacja
-      <PaginatedTable dataEntries={{}}/>
       <Pagination/>
     </div>
   );
