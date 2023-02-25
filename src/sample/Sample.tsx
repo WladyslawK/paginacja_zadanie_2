cb04b11b8197945fd38562eaa2fd7763f55cceab
WladyslawK <vladkurr7890@gmail.com>
2023-02-25 23:59:11 +0100

    fix(Sample) Sample refactor

--- src/sample/Sample.tsx
+++ src/sample/Sample.tsx
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react'
+import axios from "axios"
+import {usePagination} from "../hooks/usePagination"
+import PaginatedData from "../components/paginatedData/PaginatedData"
+import {Pagination} from "../components/pagination/Pagination"
+import {usePaginationLayout} from "../hooks/usePaginationLayout";
+
+export type postTpe = {
+  userId: number
+  id: number
+  title: string
+  body: string
+}
 
 export const Sample = () => {
-  return (
-    <>
 
-    </>
+  const [posts, setPosts] = useState<postTpe[]>([])
+  const [paginationState, paginationActions] = usePagination(posts)
+
+  const {actualPageIdx, entriesOnSelectedPage} = paginationState
+  const {goToFirstPage} = paginationActions
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        setPosts(res.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
+
+
+  useEffect(() => {
+      goToFirstPage()
+  }, [entriesOnSelectedPage])
+
+  const lastPostIndex = actualPageIdx * entriesOnSelectedPage
+  const firstPostIndex = lastPostIndex - entriesOnSelectedPage
+  const items = posts.slice(firstPostIndex, lastPostIndex)
+
+  const pagination = usePaginationLayout(posts.length, entriesOnSelectedPage, 1, actualPageIdx)
+
+
+  return (
+    <div>
+      <PaginatedData posts={items}/>
+      <Pagination
+        pagination={pagination}
+        paginationState={paginationState} paginationActions={paginationActions}
+      />
+    </div>
   );
 }
